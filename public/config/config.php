e7272040c4aa6f2622f1b9b7aeb1d398efb00a70
Vagg-Davios <evaggelosdavios@gmail.com>
2023-06-05 01:49:40 +0300

    misc

---
+++ public/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'studentlibrary';
+const DB_CHARSET = 'utf8mb4';
+const DB_USER = 'root';
+const DB_PASS = '';
+
+const ROOT = '/junior-development-assignment/public';
+
