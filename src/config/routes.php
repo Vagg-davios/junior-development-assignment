7984689a3076905a94d12af0345da6f4ef6582ef
Vagg-Davios <evaggelosdavios@gmail.com>
2023-06-06 23:51:25 +0300

    added register routes (get for controller load and post for form submission)

--- src/config/routes.php
+++ src/config/routes.php
@@ -6,3 +6,5 @@ $router->get('/', $BASE . "/src/controller/book/index.php");
 $router->get('/book', $BASE . "/src/controller/book/show.php");
 $router->get('/borrowed', $BASE . "/src/controller/book/borrowed.php");
 
+$router->get('/register', $BASE . "/src/controller/user/register.php");
+$router->post('/register', $BASE . "/src/controller/user/create-user.php");
